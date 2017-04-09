ab37c10fe0e487e9685bcfb64724ec0483102ee8
Ben Crist <ben@magicmoremagic.com>
2017-04-09 17:37:29 -0500

    versioning

---
+++ include/version.hpp
@@ -0,0 +1,18 @@
+#pragma once
+#ifndef BE_BELUA_VERSION_HPP_
+#define BE_BELUA_VERSION_HPP_
+
+#include <be/core/macros.hpp>
+
+#define BE_BELUA_VERSION_MAJOR 0
+#define BE_BELUA_VERSION_MINOR 1
+#define BE_BELUA_VERSION_REV 1
+
+/*!! include('common/version', 'be::belua') !! 6 */
+/* ################# !! GENERATED CODE -- DO NOT MODIFY !! ################# */
+#define BE_BELUA_VERSION (BE_BELUA_VERSION_MAJOR * 100000 + BE_BELUA_VERSION_MINOR * 1000 + BE_BELUA_VERSION_REV)
+#define BE_BELUA_VERSION_STRING "be::belua " BE_STRINGIFY(BE_BELUA_VERSION_MAJOR) "." BE_STRINGIFY(BE_BELUA_VERSION_MINOR) "." BE_STRINGIFY(BE_BELUA_VERSION_REV)
+
+/* ######################### END OF GENERATED CODE ######################### */
+
+#endif
